aa6b8b9757713d9e73fffd9b7fce5910a1f2436d
geothermal-1408 <savvy3526@gmail.com>
2024-11-26 11:54:55 +0530

    heatmap added

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,41 +1,108 @@
-"use client";
+// "use client";
 
+// import "leaflet/dist/leaflet.css";
+// import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css";
+// import "leaflet-defaulticon-compatibility";
+
+// import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
+
+// interface MapComponentProps {
+//   position: [number, number]; // Expect a tuple of two numbers
+// }
+
+// const MapComponent: React.FC<MapComponentProps> = ({ position }) => {
+//   const MapUpdater = () => {
+//     const map = useMap();
+//     map.setView(position, map.getZoom()); // Dynamically update the map's center
+//     return null;
+//   };
+
+//   return (
+//     <MapContainer
+//       center={position}
+//       zoom={10}
+//       scrollWheelZoom={true}
+//       style={{ height: "400px", width: "100%", margin: "auto" }}
+//     >
+//       <TileLayer
+//         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+//         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+//       />
+//       <Marker position={position}>
+//         <Popup>
+//           Coordinates: {position[0]}, {position[1]}
+//         </Popup>
+//       </Marker>
+//       <MapUpdater />
+//     </MapContainer>
+//   );
+// };
+
+// export default MapComponent;
+
+
+import { useEffect, useState } from "react";
+import { MapContainer, TileLayer, useMap } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css";
-import "leaflet-defaulticon-compatibility";
+import L from "leaflet";
+import "leaflet.heat";
 
-import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
+// Dynamically import Leaflet and Leaflet Heatmap
+// const L = dynamic(() =>  import('leaflet'), { ssr: false });
+// const HeatmapLayer = dynamic(() => import('leaflet.heat'), { ssr: false });
 
-interface MapComponentProps {
-  position: [number, number]; // Expect a tuple of two numbers
-}
+type HeatmapProps = {
+  heatData: [number, number, number][];
+};
 
-const MapComponent: React.FC<MapComponentProps> = ({ position }) => {
-  const MapUpdater = () => {
-    const map = useMap();
-    map.setView(position, map.getZoom()); // Dynamically update the map's center
-    return null;
+const Heatmap: React.FC<HeatmapProps> = ({ heatData }) => {
+  const [mapReady, setMapReady] = useState(false);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setMapReady(true); // Trigger map rendering after the client has loaded
+    }
+  }, []);
+
+  const MapWithHeat = () => {
+    const map = useMap(); // Get access to the map instance
+
+    useEffect(() => {
+      if (map && heatData) {
+        // Create the heatmap layer using leaflet.heat
+        const heatLayer = L.heatLayer(heatData, {
+          radius: 35,
+          blur: 15,
+          maxZoom: 17,
+        });
+
+        // Add the heatmap layer to the map
+        heatLayer.addTo(map);
+
+        // Cleanup the heatmap layer when the component is unmounted
+        return () => {
+          map.removeLayer(heatLayer);
+        };
+      }
+    }, [map, heatData]);
+
+    return null; // This component doesn’t render anything itself, it just adds the heatmap to the map
   };
+
+  if (!mapReady) return <div>Loading...</div>;
 
   return (
     <MapContainer
-      center={position}
-      zoom={10}
-      scrollWheelZoom={true}
-      style={{ height: "400px", width: "100%", margin: "auto" }}
+      center={[51.505, -0.09]}
+      zoom={13}
+      style={{ height: "38vh", width: "100%" }}
     >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={position}>
-        <Popup>
-          Coordinates: {position[0]}, {position[1]}
-        </Popup>
-      </Marker>
-      <MapUpdater />
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+
+      {/* Add Heatmap Layer */}
+      <MapWithHeat />
     </MapContainer>
   );
 };
 
-export default MapComponent;
+export default Heatmap;
