7ab92eda07423a6da5ade01addd0a5b6dad2a073
Davygupta47 <dwaipayan.dg07@gmail.com>
2024-12-05 02:00:24 +0530

    logsign

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -106,141 +106,154 @@ export default function SignUp() {
   };
 
   return (
-    <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black">
-      <h2 className="font-bold text-xl text-center text-neutral-800 dark:text-neutral-200">
-        Welcome to CarbonTrack India
-      </h2>
+    <div
+    className="w-full min-h-screen flex items-center justify-center bg-cover bg-center"
+    style={{
+      backgroundImage:
+        "url('https://www.etifor.com/en/wp-content/uploads/2022/12/img_Carbon-neutrality.jpg')",
+      backgroundSize: "cover", // Ensures the image fills the screen
+      backgroundPosition: "center", // Keeps the image centered
+      backgroundRepeat: "no-repeat", // Prevents image tiling
+      imageRendering: "crisp-edges", // Ensures a sharp image
+    }}
+  >
+      <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white bg-opacity-80 dark:bg-black dark:bg-opacity-80">
+        <h2 className="font-bold text-xl text-center text-neutral-800 dark:text-neutral-200">
+          Welcome to CarbonTrack India
+        </h2>
 
-      <p className="text-center mt-4">
-        Already a member?
-        <Link
-          href={"/login"}
-          className="font-bold text-red-600 hover:text-blue-500"
-        >
-          Login
-        </Link>
-      </p>
-      <form className="my-8" onSubmit={handleSubmit}>
-        <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
-          <LabelInputContainer>
-            <Label htmlFor="firstname">First name</Label>
+        <p className="text-center mt-4">
+          Already a member?
+          <Link
+            href={"/login"}
+            className="font-bold text-red-600 hover:text-blue-500"
+          >
+            Login
+          </Link>
+        </p>
+        <form className="my-8" onSubmit={handleSubmit}>
+          <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
+            <LabelInputContainer>
+              <Label htmlFor="firstname">First name</Label>
+              <Input
+                id="firstname"
+                placeholder="Tony"
+                type="text"
+                name="firstname"
+                value={formData.firstname}
+                onChange={handleChange}
+              />
+              {errors.firstname && (
+                <small className="text-red-500">{errors.firstname}</small>
+              )}
+            </LabelInputContainer>
+            <LabelInputContainer>
+              <Label htmlFor="lastname">Last name</Label>
+              <Input
+                id="lastname"
+                placeholder="Stark"
+                type="text"
+                name="lastname"
+                value={formData.lastname}
+                onChange={handleChange}
+              />
+              {errors.lastname && (
+                <small className="text-red-500">{errors.lastname}</small>
+              )}
+            </LabelInputContainer>
+          </div>
+          <LabelInputContainer className="mb-4">
+            <Label htmlFor="email">Email Address</Label>
             <Input
-              id="firstname"
-              placeholder="Tony"
-              type="text"
-              name="firstname"
-              value={formData.firstname}
+              id="email"
+              placeholder="ironman@gmail.com"
+              name="email"
+              type="email"
+              value={formData.email}
               onChange={handleChange}
             />
-            {errors.firstname && (
-              <small className="text-red-500">{errors.firstname}</small>
+            {errors.email && (
+              <small className="text-red-500">{errors.email}</small>
             )}
           </LabelInputContainer>
-          <LabelInputContainer>
-            <Label htmlFor="lastname">Last name</Label>
+          <LabelInputContainer className="mb-4">
+            <Label htmlFor="password">Password</Label>
             <Input
-              id="lastname"
-              placeholder="Stark"
-              type="text"
-              name="lastname"
-              value={formData.lastname}
+              id="password"
+              placeholder="••••••••"
+              name="password"
+              type="password"
+              value={formData.password}
               onChange={handleChange}
             />
-            {errors.lastname && (
-              <small className="text-red-500">{errors.lastname}</small>
+            {errors.password && (
+              <small className="text-red-500">{errors.password}</small>
             )}
           </LabelInputContainer>
+          <LabelInputContainer className="mb-8">
+            <Label htmlFor="confirmpassword">Confirm Password</Label>
+            <Input
+              id="confirmPassword"
+              placeholder="••••••••"
+              name="confirmPassword"
+              type="password"
+              value={formData.confirmPassword}
+              onChange={handleChange}
+            />
+            {errors.confirmPassword && (
+              <small className="text-red-500">{errors.confirmPassword}</small>
+            )}
+          </LabelInputContainer>
+
+          <button
+            className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset] hover:scale-105 transition-transform"
+            type="submit"
+          >
+            Sign up &rarr;
+            <BottomGradient />
+          </button>
+        </form>
+        <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
+
+        <div className="flex flex-col space-y-4">
+          <button
+            className=" relative group/btn hover:scale-105 transition-transform flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
+            type="submit"
+            onClick={() => signIn("github", { callbackUrl: "/dashboard" })}
+          >
+            <IconBrandGithub className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
+            <span className="text-neutral-700 dark:text-neutral-300 text-sm">
+              GitHub
+            </span>
+            <BottomGradient />
+          </button>
+          <button
+            className=" relative group/btn hover:scale-105 transition-transform flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
+            type="submit"
+            onClick={() => signIn("google", { callbackUrl: "/dashboard" })}
+          >
+            <IconBrandGoogle className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
+            <span className="text-neutral-700 dark:text-neutral-300 text-sm">
+              Google
+            </span>
+            <BottomGradient />
+          </button>
+          <button
+            className=" relative group/btn hover:scale-105 transition-transform flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
+            type="submit"
+          >
+            <IconBrandFacebook className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
+            <span className="text-neutral-700 dark:text-neutral-300 text-sm">
+              Facebook
+            </span>
+            <BottomGradient />
+          </button>
         </div>
-        <LabelInputContainer className="mb-4">
-          <Label htmlFor="email">Email Address</Label>
-          <Input
-            id="email"
-            placeholder="ironman@gmail.com"
-            name="email"
-            type="email"
-            value={formData.email}
-            onChange={handleChange}
-          />
-          {errors.email && (
-            <small className="text-red-500">{errors.email}</small>
-          )}
-        </LabelInputContainer>
-        <LabelInputContainer className="mb-4">
-          <Label htmlFor="password">Password</Label>
-          <Input
-            id="password"
-            placeholder="••••••••"
-            name="password"
-            type="password"
-            value={formData.password}
-            onChange={handleChange}
-          />
-          {errors.password && (
-            <small className="text-red-500">{errors.password}</small>
-          )}
-        </LabelInputContainer>
-        <LabelInputContainer className="mb-8">
-          <Label htmlFor="confirmpassword">Confirm Password</Label>
-          <Input
-            id="confirmPassword"
-            placeholder="••••••••"
-            name="confirmPassword"
-            type="password"
-            value={formData.confirmPassword}
-            onChange={handleChange}
-          />
-          {errors.confirmPassword && (
-            <small className="text-red-500">{errors.confirmPassword}</small>
-          )}
-        </LabelInputContainer>
-
-        <button
-          className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset] hover:scale-105 transition-transform"
-          type="submit"
-        >
-          Sign up &rarr;
-          <BottomGradient />
-        </button>
-      </form>
-      <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
-
-      <div className="flex flex-col space-y-4">
-        <button
-          className=" relative group/btn hover:scale-105 transition-transform flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
-          type="submit"
-          onClick={() => signIn("github", { callbackUrl: "/dashboard" })}
-        >
-          <IconBrandGithub className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
-          <span className="text-neutral-700 dark:text-neutral-300 text-sm">
-            GitHub
-          </span>
-          <BottomGradient />
-        </button>
-        <button
-          className=" relative group/btn hover:scale-105 transition-transform flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
-          type="submit"
-          onClick={() => signIn("google", { callbackUrl: "/dashboard" })}
-        >
-          <IconBrandGoogle className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
-          <span className="text-neutral-700 dark:text-neutral-300 text-sm">
-            Google
-          </span>
-          <BottomGradient />
-        </button>
-        <button
-          className=" relative group/btn hover:scale-105 transition-transform flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
-          type="submit"
-        >
-          <IconBrandFacebook className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
-          <span className="text-neutral-700 dark:text-neutral-300 text-sm">
-            Facebook
-          </span>
-          <BottomGradient />
-        </button>
       </div>
     </div>
   );
-}
+};
+
 const BottomGradient = () => {
   return (
     <>
@@ -249,6 +262,7 @@ const BottomGradient = () => {
     </>
   );
 };
+
 const LabelInputContainer = ({
   children,
   className,
